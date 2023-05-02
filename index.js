f0e13b5b3c274d614e92206b4cf57fe8d158b411
Felix Gomez <fgomez@binwus.com>
2023-05-02 19:48:34 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,294 @@
+
+const resume = {
+/*  en: 
+  {
+    summary: {
+        title: "Summary",
+        info:
+          "Software Engineer with over 8 years of experience in web development, specializing in REST APIs, microservices, and full-stack developments. Strong problem-solving skills and a versatile mindset that enables effective collaboration with various individuals.",
+      },
+      technicalSkills: {
+        title: "Technical Skills",
+        info: [
+          "Programming Languages: JavaScript, C#, Scala, Java, Swift, Rust, Python",
+          "Web Technologies: React, Angular, HTML5, Responsive Design",
+          "Agile Methodologies: Scrum, Kanban",
+          "Databases: RDBMS, MongoDB, CosmosDB",
+          "Version Control: Git",
+        ]
+      },
+      professionalExperience: {
+        title: "Professional Experience",
+        info: [
+          {
+            position: "Software Developer",
+            company: "Servieduca",
+            location: "[Company_Location]",
+            period: "July 2012 – April 2016",
+            responsibilities: [
+              "Designed and supported multiple websites for clients, resulting in a 15% increase in user engagement",
+              "Performed bug fixing, enhanced existing features, and developed new features",
+            ],
+          },
+          {
+            position: "Senior Software Developer",
+            company: "CPS",
+            location: "[Company_Location]",
+            period: "April 2016 – October 2017",
+            responsibilities: [
+              "Developed and designed custom technological solutions for various clients, improving organizational efficiency by 20%",
+              "Improved organizational systems through new feature development and bug resolution",
+            ],
+          },
+          {
+            position: "Senior Software Developer",
+            company: "Hexacta",
+            location: "[Company_Location]",
+            period: "October 2017 – December 2018",
+            responsibilities: [
+              "Created innovative technological solutions for clients while providing support to existing features",
+              "Enhanced existing features, developed new ones, and resolved bugs, reducing system downtime by 25%",
+            ],
+          },
+          {
+            position: "Software Developer",
+            company: "Binwus",
+            location: "[Company_Location]",
+            period: "January 2021 – September 2021",
+            responsibilities: [
+              "Developed and designed custom technological solutions for various clients, leading to a 30% increase in system performance",
+              "Improved organizational systems through new feature development and bug resolution",
+            ],
+          },
+          {
+            position: "Scala Developer",
+            company: "Applaudo Studios",
+            location: "[Company_Location]",
+            period: "September 2021 – Present",
+            responsibilities: [
+              "Constructed highly concurrent backend reactive microservices for processing large volumes of data using Kafka streaming",
+              "Designed creative technological solutions to address organizational challenges",
+            ],
+          },
+        ],
+      },
+    education: {
+        title: "Education",
+        info: "Bachelor of Computer Science Engineering, 2012 Universidad Rafael Belloso Chacin, Maracaibo, Venezuela"
+    },
+    certifications: {
+        title: "Certifications and Additional Courses",
+        info: [
+            "Introduction to Artificial Intelligence, Stanford Online Course, 2010",
+            "Functional Programming Principles in Scala, Coursera, 2013",
+            "Principles of Reactive Programming, Coursera, 2014",
+            " Introduction to Mathematical Thinking, Stanford Coursera, 2015"]
+    },
+    projects:{ 
+        title:"Projects",
+        info:[
+        {
+          name: "Adaptation of Microsoft Dynamics CRM for a university system and website integration",
+          role: "C# Developer and CRM Expert",
+          technologies: ["Microsoft Dynamics CRM", "C#", "Razor", ".NET 4.5", "IIS", "SQL Server"]
+        },
+        {
+          name: "Creation of a distributed system to handle the financial information of multiple stores across a country in near real-time",
+          role: "Scala Developer",
+          technologies: ["Scala", "Kafka", "Akka", "Akka Streams", "Kubernetes", "DB2", "CosmosDB", "Azure Blob Storage"]
+        }]
+    },
+    languages: {
+        title: "Languages",
+        info: "English Advanced \n Spanish Native",
+      },
+    location: { 
+        title: "Location",
+        info: "Person Location"
+    }
+  },
+  es: {
+    summary: {
+      title: "Resumen",
+      info:
+        "Ingeniero de Software con más de 8 años de experiencia en desarrollo web, especializado en APIs REST, microservicios y full-stack development. Fuertes habilidades para resolver problemas y mentalidad versátil que permite una colaboración efectiva con diversas personas.",
+    },
+    technicalSkills: {
+      title: "Habilidades Técnicas",
+      info: [
+        "Lenguajes de Programación: JavaScript, C#, Scala, Java, Swift",
+        "Tecnologías Web: React, Angular, HTML5, Diseño Responsivo",
+        "Metodologías Ágiles: Scrum, Kanban",
+        "Bases de Datos: RDBMS, MongoDB, CosmosDB",
+        "Control de Versiones: Git",
+      ],
+    },
+    professionalExperience: {
+      title: "Experiencia Profesional",
+      info: [
+        {
+          position: "Desarrollador de Software",
+          company: "Servieduca",
+          location: "[Ubicación_de_la_empresa]",
+          period: "Julio 2012 – Abril 2016",
+          responsibilities:[
+            "Diseñó y brindó soporte a múltiples sitios web para clientes, lo que resultó en un aumento del 15% en la participación de los usuarios\n• Realizó correcciones de errores, mejoró las funciones existentes y desarrolló nuevas funciones",
+          ]
+        },
+        {
+          position: "Desarrollador Senior de Software",
+          company: "CPS",
+          location: "[Ubicación_de_la_empresa]",
+          period: "Abril 2016 – Octubre 2017",
+          responsibilities:[
+            "Desarrolló y diseñó soluciones tecnológicas personalizadas para diversos clientes, mejorando la eficiencia organizativa en un 20%\n• Mejoró los sistemas organizativos mediante el desarrollo de nuevas funciones y la resolución de errores",
+          ]
+        },
+        {
+          position: "Desarrollador Senior de Software",
+          company: "Hexacta",
+          location: "[Ubicación_de_la_empresa]",
+          period: "Octubre 2017 – Diciembre 2018",
+          responsibilities:[
+            "Creó soluciones tecnológicas innovadoras para clientes y brindó soporte a las funciones existentes\n• Mejoró las funciones existentes, desarrolló nuevas y resolvió errores, reduciendo el tiempo de inactividad del sistema en un 25%",
+          ]
+        },
+        {
+          position: "Desarrollador de Software",
+          company: "Binwus",
+          location: "[Ubicación_de_la_empresa]",
+          period: "Enero 2021 – Septiembre 2021",
+          responsibilities:[
+            "Desarrolló y diseñó soluciones tecnológicas personalizadas para diversos clientes, lo que llevó a un aumento del 30% en el rendimiento del sistema\n• Mejoró los sistemas organizativos mediante el desarrollo de nuevas funciones y la resolución de errores"
+            ]
+        },
+        {
+          position: "Desarrollador de Scala",
+          company: "Applaudo Studios",
+          location: "[Ubicación_de_la_empresa]",
+          period: "Septiembre 2021 – Presente",
+          responsibilities:[
+            "Construyó microservicios reactivos de backend altamente concurrentes para procesar grandes volúmen  menes de datos utilizando transmisión de Kafka\n• Diseñó soluciones tecnológicas creativas para abordar desafíos organizativos"
+            ]
+        },
+      ],
+    },
+    education: {
+      title: "Educación",
+      info:
+        "Licenciatura en Ingeniería en Informática, 2012\nUniversidad Rafael Belloso Chacín, Maracaibo, Venezuela",
+    },
+    certifications: {
+      title: "Certificaciones y Cursos Adicionales",
+      info: [
+        "Introducción a la Inteligencia Artificial, Curso en línea de Stanford, 2010",
+        "Principios de Programación Funcional en Scala, Coursera, 2013",
+        "Principios de Programación Reactiva, Coursera, 2014",
+        "Introducción al Pensamiento Matemático, Coursera de Stanford, 2015",
+      ],
+    },
+    projects: {
+      title: "Proyectos destacados",
+      info: [
+        {
+          name: "Adaptación de Microsoft Dynamics CRM para un sistema universitario e integración de sitio web",
+          role: "Desarrollador de C# y Experto en CRM",
+          technologies:
+            "Microsoft Dynamics CRM, C#, Razor, .NET 4.5, IIS, SQL Server",
+        },
+        {
+          name: "Creación de un sistema distribuido para manejar la información financiera de múltiples tiendas en todo un país en tiempo casi real",
+          role: "Desarrollador de Scala",
+          technologies:
+            "Scala, Kafka, Akka, Akka Streams, Kubernetes, DB2, CosmosDB, Azure Blob Storage",
+        },
+      ],
+    },
+    languages: {
+      title: "Idiomas",
+      info: "Español: Nativo\nInglés: Avanzado",
+    },
+    location: {
+      title: "Ubicación",
+      info: "[Ubicación_de_la_persona]",
+    },
+  },*/
+};
+
+let currentLanguage = "en";
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadLanguageFiles().then(() => {
+    updateLanguage();
+  });
+});
+
+async function loadLanguageFiles() {
+  const enResponse = await fetch("./resume-en.json");
+  const esResponse = await fetch("./resume-es.json");
+  resume["en"] = await enResponse.json();
+  resume["es"] = await esResponse.json();
+}
+
+
+function changeLanguage(lang) {
+    currentLanguage = lang;
+    updateLanguage();
+  
+    const enBtn = document.getElementById("btnEnglish");
+    const esBtn = document.getElementById("btnSpanish");
+  
+    if (currentLanguage === "en") {
+      enBtn.disabled = true;
+      esBtn.disabled = false;
+    } else {
+      enBtn.disabled = false;
+      esBtn.disabled = true;
+    }
+  }
+
+function createListItems(dataArray) {
+    return dataArray.map((item) => `<li>${item}</li>`).join("");
+  }
+  
+  function createExperienceListItems(dataArray) {
+    return dataArray
+      .map(
+        (experience) =>
+          `<li><strong>${experience.position} | ${experience.company}, ${experience.location}</strong><br>${experience.period}<br>${experience.responsibilities
+            .map((responsibility) => `• ${responsibility}`)
+            .join("<br>")}</li>`
+      )
+      .join("");
+  }
+  
+  function createProjectListItems(dataArray) {
+    return dataArray
+      .map(
+        (project) =>
+          `<li><strong>${project.name}</strong><br>${project.role}<br>Tecnologías: ${project.technologies}</li>`
+      )
+      .join("");
+  }
+  
+  function updateSection(sectionId, titleKey, infoKey, listItemsFn) {
+    const lang = currentLanguage;
+    const section = resume[lang][sectionId];
+    document.getElementById(`${sectionId}Title`).innerHTML = section[titleKey];
+    if (sectionId === "summary" || sectionId === "education"  || sectionId === "location" || sectionId === "languages"  ) {
+        document.getElementById(`${sectionId}Info`).innerHTML = section[infoKey];
+      } else {
+        document.getElementById(`${sectionId}List`).innerHTML = listItemsFn(section[infoKey]);
+      }
+  }
+  
+  function updateLanguage() {
+    updateSection("summary", "title", "info", (info) => info);
+    updateSection("technicalSkills", "title", "info", createListItems);
+    updateSection("professionalExperience", "title", "info", createExperienceListItems);
+    updateSection("education", "title", "info", (info) => info);
+    updateSection("certifications", "title", "info", createListItems);
+    updateSection("projects", "title", "info", createProjectListItems);
+    updateSection("languages", "title", "info", (info) => info);
+    updateSection("location", "title", "info", (info) => info);
+  }
